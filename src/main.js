be4c231e48e6366f400e63fb60816a0e8b6cf5cd
matteocenci <matteocenci01@gmail.com>
2024-05-14 15:44:56 +0200

    Import: popper.js and dropdown works

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import "./style/general.scss";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.vue';
 import { router } from './router';
 
