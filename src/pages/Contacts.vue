1212c464e006d8858d672b301cc633efb8872b32
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-14 16:34:00 +0200

    fix: AppBusiness

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,10 +1,12 @@
 <script>
+import AppBusiness from "../components/AppBusiness.vue";
 import AppFormVue from "../components/AppForm.vue";
 import AppSubscribe from "../components/AppSubscribe.vue";
 import { store } from '../store';
 export default {
     components: {
-        AppFormVue, AppSubscribe
+        AppFormVue, AppSubscribe,
+        AppBusiness
     },
     data() {
         return {
@@ -24,13 +26,13 @@ export default {
     <!-- /contact-us section -->
     <!-- newsletter section -->
     <section class="ms_newsletter container d-flex mb-3 mt-3">
-        <div class="p-5">
+        <div class="p-5 ps-0">
             <p class="ms_send">NEWSLETTER</p>
             <h3 class="fs-1 fw-bold">Know First</h3>
             <p class="ms_respond">Follow closely and receive content about our company <br>and the news of the current
                 market</p>
         </div>
-        <div class="flex-grow-1 p-5">
+        <div class="flex-grow-1 p-5 pe-0">
             <AppSubscribe />
         </div>
     </section>
@@ -41,8 +43,9 @@ export default {
     </section>
     <!-- /business section -->
     <div class="container">
-       <AppFormVue /> 
-    </div>  
+        <AppBusiness :store="store.contacts" />
+        <AppFormVue />
+    </div>
 </template>
 
 <style scoped lang="scss">
@@ -55,6 +58,7 @@ export default {
     background-size: cover;
     padding: 100px;
     position: relative;
+
     h1 {
         color: white;
         font-weight: 900;
@@ -67,14 +71,15 @@ export default {
         color: $primary-color;
     }
 }
+
 .ms_contact::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,0.3);
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
 }
 
 .ms_newsletter {
@@ -86,10 +91,4 @@ export default {
         color: $form-text;
     }
 }
-
-.business-section {
-    background-color: #111117;
-    height: 350px;
-}
-
 </style>
