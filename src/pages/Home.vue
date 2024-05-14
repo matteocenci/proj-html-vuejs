b556dc68f20b912598903c705ac5f885006f7734
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-05-14 09:24:33 +0200

    add: mixin flex

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,9 +5,18 @@
 </script>
 
 <template>
-    <h1>Home</h1>
+    <section class="hero">
+        <h4>LOGISTIC SERVICES</h4>
+        <h2>CARGO TRANSPORT</h2>
+        <p>Fractional or exclusive road cargo transportation to all regions, with small, medium and large vehicles.</p>
+    </section>
 </template>
 
 <style scoped lang="scss">
+
+.hero {
+    height: 100vh;
+    background-image: url(../assets/img/bg-9.jpg);
+}
 </style>
 
