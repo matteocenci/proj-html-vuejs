0284402446e680a4336719f2e9117e9f1283ccfa
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-05-14 09:49:37 +0200

    add: class utilities for button

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,36 @@
 <script>
-    export default {
+export default {
 
-    }   
+}   
 </script>
 
 <template>
     <section class="hero">
-        <h4>LOGISTIC SERVICES</h4>
-        <h2>CARGO TRANSPORT</h2>
-        <p>Fractional or exclusive road cargo transportation to all regions, with small, medium and large vehicles.</p>
+        <div class="dark">
+            <h4>LOGISTIC SERVICES</h4>
+            <h2>CARGO TRANSPORT</h2>
+            <p>Fractional or exclusive road cargo transportation to all regions, with small, medium and large vehicles.</p>
+            <button class="btn-prymary-color">GET IN TOUCH</button>
+            <button class="btn-secondary-color">READ MORE</button>
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as *;
 
 .hero {
-    height: 100vh;
-    background-image: url(../assets/img/bg-9.jpg);
+
+    .dark {
+        width: 100%;
+        @include flex(column, center, flex-start);
+        height: 100vh;
+        background-image: url(../assets/img/bg-9.jpg);
+
+        h4 {
+            color: $primary-color;
+        }
+    }
 }
 </style>
-
