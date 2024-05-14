8d189133bdc2a713d6f75d23065224617969eec4
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-14 11:13:56 +0200

    feat: form in components

---
+++ src/components/AppForm.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="container p-0">
+        <p class="ms_send">SEND A MESSAGE</p>
+        <h2 class="fw-bold fs-1">Get in Touch</h2>
+        <p class="ms_respond">We will respond to your message as soon as possible</p>
+        <div class="d-flex justify-content-between gap-4">
+            <div class="flex-grow-1">
+                <label for="exampleFormControlInput1" class="form-label"></label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Name">
+                <label for="exampleFormControlInput1" class="form-label"></label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Phone">
+
+            </div>
+            <div class="flex-grow-1">
+                <label for="exampleFormControlInput1" class="form-label"></label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                <label for="exampleFormControlInput1" class="form-label"></label>
+                <input type="email" class="form-control" id="exampleFormControlInput1"
+                    placeholder="--Please choose an option--">
+
+            </div>
+        </div>
+        <div class="mb-3 flex-grow-1">
+            <label for="exampleFormControlTextarea1" class="form-label"></label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Message</textarea>
+        </div>
+
+        <button class="btn-prymary-color">SEND</button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/mixins.scss' as *;
+@use '../style/partials/variables.scss' as *;
+
+input,
+textarea {
+    background-color: $bg-form;
+    color: $form-text;
+    padding: 13px;
+    border-radius: 3px;
+}
+
+.ms_send {
+    color: $primary-color;
+}
+
+.ms_respond {
+    color: $form-text;
+}
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,8 +1,14 @@
 <script>
+import AppFormVue from "../components/AppForm.vue"
+export default {
+    components: {
+        AppFormVue
+    }
+}
 </script>
 
 <template>
-
+<AppFormVue />
 </template>
 
 <style scoped lang="scss">
