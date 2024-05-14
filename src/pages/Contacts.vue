dbfe885cd8a189f420661aec82e7a559d5600a04
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-14 11:44:07 +0200

    feat: contact section in contacts

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -8,8 +8,27 @@ export default {
 </script>
 
 <template>
+    <!-- contact-us section -->
+    <section class="ms_contact d-flex justify-content-center">
+        <h1 class="align-content-center">Contact Us</h1>
+    </section>
 <AppFormVue />
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/mixins.scss' as *;
+@use '../style/partials/variables.scss' as *;
+
+.ms_contact {
+    background-image: url(../assets/img/bg-1.jpg);
+    height: 350px;
+    background-position: center;
+    background-size: cover;
+
+    h1 {
+        color: white;
+        font-weight: 900;
+        font-size: 4rem;
+    }
+}
 </style>
