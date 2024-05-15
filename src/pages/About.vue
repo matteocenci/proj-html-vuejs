32bc190a737263bfacb0097461f4d356985ffe7c
matteocenci <matteocenci01@gmail.com>
2024-05-15 11:30:19 +0200

    feat: components WhoWeAre

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,11 +1,18 @@
 <script>
+import WhoWeAre from "../components/WhoWeAre.vue";
+
+
     export default {
+        components: {
+            WhoWeAre,
+        }
+        
 
     }   
 </script>
 
 <template>
-    <h2>About</h2>
+    <WhoWeAre />
 </template>
 
 <style scoped lang="scss">
