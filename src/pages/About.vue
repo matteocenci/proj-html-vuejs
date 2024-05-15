c56c59ee24dbc4a461fb831fc62ea6924056bccb
matteocenci <matteocenci01@gmail.com>
2024-05-15 17:41:11 +0200

    fixing: About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,6 +2,7 @@
 import WhoWeAre from "../components/WhoWeAre.vue";
 import AppHero from "../components/AppHero.vue";
 import CompanySection from "../components/CompanySection.vue";
+import ResultsSection from "../components/ResultsSection.vue";
 import {store} from "../store.js";
 
 
@@ -10,6 +11,7 @@ import {store} from "../store.js";
             WhoWeAre,
             AppHero,
             CompanySection,
+            ResultsSection
         },
         data() {
             return {
@@ -24,10 +26,12 @@ import {store} from "../store.js";
 <template>
     <AppHero :hero="store.heroSection[0]" class="app-hero-1"/>
     <WhoWeAre />
+    <ResultsSection :results="store.results"/>
     <CompanySection />
 </template>
 
 <style scoped lang="scss">
+
 
 .app-hero-1 {
     background-image: url(../assets/img/bg-about.jpg);
